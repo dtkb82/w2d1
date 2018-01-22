822dd63b3b4807e42223b4c281e742ee3baf9d2b
Kenneth <krbrown@live.ca>
2018-01-22 18:29:27 +0000

    step 1: complete

--- http_request/streams/https_example2.js
+++ http_request/streams/https_example2.js
@@ -1,30 +1,46 @@
 
 // while https is built-in to Node, it is a module, so it must be required
-var https = require('https');
+
 
 // the host can be thought of as the domain name you want to read from,
 // and the path is the resource - '/' is the root path, but if you wanted to read a
 // particular resource (like '/login/index.html'), that would be defined in the path
 
-function getAndPrintHTMLChunks(response) {
+
+function getAndPrintHTMLChunks() {
+	var https = require('https');
 
 	var requestOptions = {
-	  host: 'sytantris.github.io',
-	  path: '/http-examples/step1.html'
-};
+		host: 'sytantris.github.io',
+		path: '/http-examples/step1.html'
+	};
 
-response.on('data', function (chunk) {
-	console.log('Chunk received. Length:', chunk.length + '\n');
-	console.log(chunk.toString());
-});
+	https.get(requestOptions, function (response) {
+		//set encoding of received data to UTF-8
+		response.setEncoding('utf8');
 
+   		response.on('data', function (data) {
+			console.log('Chunk Received. Length:', data.length + '\n');
+
+		})
+
+    	response.on('end', function() {
+			console.log('Response stream complete.');
+		});
+	
+
+	});
 }
 
-console.log("I'm about to make the request!");
+getAndPrintHTMLChunks();
 
-https.request(requestOptions, callback).end();
 
-console.log("I've made the request!");
+
+// console.log("I'm about to make the request!");
+
+// https.request(requestOptions, callback).end();
+
+// console.log("I've made the request!");
 
 
 
