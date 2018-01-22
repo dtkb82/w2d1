7e325653dd0b87b00c50d1a5af307b64f56bc2b0
Kenneth <krbrown@live.ca>
2018-01-22 18:53:11 +0000

    step:2 buffering the chunks

--- http_request/file2.js
+++ http_request/file2.js
@@ -1,12 +1,25 @@
-function getAndPrintHTML () {
 
-  var requestOptions = {
-    host: 'sytantris.github.io',
-    path: '/http-examples/step2.html'
-  };
+function getAndPrintHTML() {
+	var https = require('https');
+	var string = "";
 
-  var filePath = process.argv.splice(2);
-    filePath = filePath.toString();
-  var buf = new Buffer(1024);
 
+	var requestOptions = {
+		host: 'sytantris.github.io',
+		path: '/http-examples/step1.html'
+	};
+
+	https.get(requestOptions, function (response) {
+		//set encoding of received data to UTF-8
+		response.setEncoding('utf8');
+
+   		response.on('data', function (data) {
+   			string += data;
+			console.log('\n', data);
+
+		})	
+
+	});
 }
+
+getAndPrintHTML();
